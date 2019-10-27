16609a8f5daf3876826a4d360b1c21d958fa16ef
Alison <alison-tang@berkeley.edu>
2019-10-26 20:43:20 -0700

    parsed some club names and descriptions

--- group_clubs.py
+++ group_clubs.py
@@ -1,25 +1,66 @@
 # Cal Hacks 6.0
 # obtain URLs for all organizations
+import urllib.request
+import sys, os, csv
 from googlesearch import search
-urls = search('site:callink.berkeley.edu inurl:organization/', stop=20)
+# urls = search('site:callink.berkeley.edu inurl:organization/', stop=20,num=10,pause=2.0)
 
 # open URLs and read in descriptions
+
+def remove_html_tags(text):
+	"""Remove html tags from a string"""
+	import re
+	clean = re.compile('<.*?>')
+	return re.sub(clean, ' ', text)
+
 club_info = {}
-for url in urls:
-	page = read(urllib.request(url))
-	print(page)
-	print(url)
-	sys.exit()
+pages = []
+# for url in urls:
+# for url in open('club_urls.txt'):
+# 	try:
+# 		page = str(urllib.request.urlopen(url).read())
+# 	except:
+# 		sys.stderr.write("Can't open {}\n".format(url))
+# 		continue
+# 	pages += [page]
+# 	name = page[page.find('"name"')+8:]
+# 	name = name[:name.find('",')]
+# 	name = name.replace('\\', '')
 
+# 	description = page[page.find('"description"')+15:]
+# 	if 'description' not in page:
+# 		continue
+# 	if '",' not in description:
+# 		end = 500
+# 	else:
+# 		end = description.find('",')
+# 	description = remove_html_tags(description[:end])
+# 	description = description.replace('\\r\\','')
+# 	description = description.replace('\\n','')
+# 	description = description.replace('\\','')
+# 	description = description.replace('&nbsp;','')	
+# 	club_info[name] = description
 
+# with open('club_html_pages.txt', 'wt') as outfile:
+# 	writer = csv.writer(outfile, delimiter='\t', lineterminator=os.linesep)
+# 	for p in pages:
+# 		writer.writerow([p])
 
+# with open('club_descriptions.txt', 'wt') as outfile:
+# 	writer = csv.writer(outfile, delimiter='\t', lineterminator=os.linesep)
+# 	for name in club_info:
+# 		writer.writerow([name, club_info[name]])
 
-# 
+for line in open('club_descriptions.txt'):
+	name, description = line.rstrip().split('\t')
+	club_info[name] = description
 
+# calculating similarity of club descriptions and then clustering clubs by their similarities
+# these libraries are pip installable 
 from textblob import TextBlob
 from sklearn.cluster import SpectralClustering
-from gensim import corpora, models, similarities
-from collections import defaultdict
+# from gensim import corpora, models, similarities
+# from collections import defaultdict
 
 def normalize(vec):
 	val = max(vec) or 1
@@ -45,39 +86,29 @@ for club in club_names:
 
 	club_description_textblob = TextBlob(club_description)
 	club_sentiment[club] = club_description_textblob.sentiment.polarity
-	club_summary[club] = set(club_description_textblob.noun_phrases)
+	club_summary[club] = set(club_description_textblob.words)  # noun_phrases attribute is nice too
+	club_summary[club] = club_summary[club] - stoplist  # get rid of common words
 
-summary_matrix = []
-for club_a in club_names:
+print(club_summary)  # dictionary of club name to description 
+
+summary_matrix = []  # making a similarity matrix
+for club_a in club_names:  # compare the similarity of club a to all other clubs b
 	row = []
 	for club_b in club_names:
 		# row += [similarities.MatrixSimilarity(club_lsi[club_a][club_lsi[club_b]])]
-		row += [(len(club_summary[club_a].intersection(club_summary[club_b])))]
+		row += [(len(club_summary[club_a].intersection(club_summary[club_b])))]  # number of overlapping words
 	summary_matrix += [normalize(row)]
 
-print(summary_matrix)
-clustering = SpectralClustering(n_clusters=2).fit(summary_matrix)
-# f = clustering.fit_predict(summary_matrix)
-f = clustering.get_params()
-print(clustering.labels_)
-print(clustering.affinity_matrix_)
-print(f)
-club_info = {'Student Environmental Resource Center':'The Student Environmental Resource Center (SERC) cultivates a collaborative space to strengthen the collective effectiveness of the sustainability community, and provides resources for students to actualize their visions of a more equitable, socially just, and resilient future. Learn more at serc.berkeley.edu.',
-'The Green Initiative Fund':'''Do you...
-Need funding for your campus sustainability project?
-Want to know what projects have been funded by TGIF?
-Want to serve on a committee that awards sustainability grants?
-Need a student internship that will provide you with green job skills?
-Want to learn more about UC Berkeleys campus sustainability efforts?
-Need information regarding starting a green fund at your institution?
-If yes, then The Green Initiative Fund is for you!
-The Green Initiative Fund (TGIF) provides funding for projects that reduce UC Berkeleys negative impact on the environment and make UC Berkeley more sustainable. TGIF allocates funds to projects that promote sustainable modes of transportation, increase energy and water efficiency, restore habitat, promote environmental and food justice, and reduce the amount of waste created by UC Berkeley. Portions of the fund also support education & behavior change initiatives, student aid (via return to aid), and student internships. Students, faculty, and staff may submit project proposals, which will be selected for funding by an annually appointed grant-making committee (TGIF Committee), consisting of students, faculty, and staff, on which the students have the majority vote.
-Learn more at http://tgif.berkeley.edu. and fat dogs!''',
-'3DMC':'''3DMC focuses on teaching and learning 3D printing and modeling skills. We hold workshops, participate in competitions, teach DeCals, and host an annual 24-hour 3D printing designathon.  We also tinker with 3D printers and 3D print cool projects!
+clustering = SpectralClustering(n_clusters=20).fit(summary_matrix)
 
-If you are interested in learning more about 3D modeling/printing, teaching it to other students, or competing in design competitions, then swing by one of our meetings or check out our 3D Printing and Design decal!
+modecluster = max(clustering.labels_, key=list(clustering.labels_).count)
 
-Meetings are every Tuesday 7-8 PM in the Makerspace (1st floor Moffitt Library) and fat dogs.'''}
-
+with open('club_cluster_results.txt', 'wt') as outfile:
+	writer = csv.writer(outfile, delimiter='\t', lineterminator=os.linesep)
+	writer.writerow(['Organization', 'Group_number'])
+	for i in range(len(clustering.labels_)):
+		writer.writerow([club_names[i], clustering.labels_[i]])
+		if clustering.labels_[i] == modecluster:
+			print(club_names[i], club_summary[club_names[i]])
 
 
